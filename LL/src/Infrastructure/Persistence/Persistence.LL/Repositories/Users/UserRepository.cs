8b80a56bcf456c372a348901327f1f541aeb2fdd
MartinUggerholm <hrhoej@hotmail.com>
2024-09-19 21:13:03 +0200

    s

--- LL/src/Infrastructure/Persistence/Persistence.LL/Repositories/Users/UserRepository.cs
+++ LL/src/Infrastructure/Persistence/Persistence.LL/Repositories/Users/UserRepository.cs
@@ -11,7 +11,7 @@ public class UserRepository : IUserRepository
         _context = context;
     }
 
-    // inheritdocs />
+    // inheritdoc />
     public bool DoesUserExist(string email)
     {
         return _context.Users.Any(x => x.Email == email);
