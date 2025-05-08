0ae01bfee092c254fde69f3ee9420d43842a06e6
Dryp <hrhoej@hotmail.com>
2025-05-08 06:50:43 +0200

    dashboard changes

--- LL/src/Presentation/admin-dashboard1/src/assets/env.js
+++
@@ -1,9 +0,0 @@
-// assets/env.js
-
-(function (window) {
-  window["env"] = window["env"] || {};
-  // Environment variables
-  window.env.environment = "dev";
-  window.env.apiBaseUrl = "https://localhost:7060";
-  window.env.isLocal = "true";
-})(this);
---
+++ LL/src/Presentation/dashboard/src/app/features/creatures/creatures.component.ts
@@ -0,0 +1,117 @@
+import { Component, OnInit } from '@angular/core';
+import { Creature } from '../../shared/models/Dtos/creature';
+import { AttributeDto } from '../../shared/models/Dtos/attributesDto';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CreatureService } from '../../core/services/api/creatures/creature.service';
+import { AttributeType } from '../../shared/models/enums/attributeType';
+import { SplitCamelCasePipe } from '../../shared/pipes/attributes/split-camel-case/split-camel-case.pipe';
+import { CommonModule, NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-creatures',
+  standalone: true,
+  imports: [
+    SplitCamelCasePipe,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgFor,
+  ],
+  templateUrl: './creatures.component.html',
+  styleUrl: './creatures.component.css',
+})
+export class CreaturesComponent implements OnInit {
+  creatures: Creature[] = [];
+  selectedCreature: Creature | null = null;
+  creatureForm!: FormGroup;
+
+  attributeKeys = Object.keys(AttributeType).filter((key) =>
+    isNaN(Number(key)),
+  );
+
+  constructor(
+    private creatureService: CreatureService,
+    private fb: FormBuilder,
+  ) {}
+
+  ngOnInit(): void {
+    this.creatureService.getCreatures().subscribe((creatures) => {
+      this.creatures = creatures;
+    });
+  }
+
+  updateForm(creature: Creature) {
+    let formControls: any = {};
+
+    formControls['level'] = new FormControl(creature.level);
+    formControls['experienceReward'] = new FormControl(
+      creature.experienceReward,
+    );
+    formControls['name'] = new FormControl(creature.name);
+
+    this.attributeKeys.forEach((key) => {
+      const foundAttribute = creature.baseAttributes.find(
+        (attr) => AttributeType[attr.attributeType] === key,
+      );
+      formControls[key] = new FormControl(
+        foundAttribute ? foundAttribute.value : 0,
+      );
+    });
+
+    this.creatureForm = this.fb.group(formControls);
+  }
+
+  saveCreature() {
+    if (this.selectedCreature) {
+      let updatedAttributes = this.attributeKeys.map((key) => ({
+        attributeType: AttributeType[key as keyof typeof AttributeType],
+        value: this.creatureForm.value[key],
+        entityId: this.selectedCreature!.id,
+      }));
+
+      this.selectedCreature.level = this.creatureForm.value.level;
+      this.selectedCreature.experienceReward =
+        this.creatureForm.value.experienceReward;
+      this.selectedCreature.baseAttributes = updatedAttributes;
+
+      this.creatureService.updateCreature(this.createNewCreature()).subscribe({
+        next: (res) => console.log('Creature updated successfully:', res),
+        error: (err) => console.error('Error updating creature:', err),
+      });
+    }
+  }
+
+  createNewCreature(): Creature {
+    const attributes: AttributeDto[] = this.attributeKeys.map((key) => ({
+      attributeType: AttributeType[key as keyof typeof AttributeType],
+      value: this.creatureForm.value[key],
+      entityId: this.selectedCreature!.id,
+    }));
+
+    const newCreature: Creature = {
+      id: this.selectedCreature!.id,
+      name: this.creatureForm.value.name, // make sure you have a 'name' control in your form
+      level: this.creatureForm.value.level,
+      experienceReward: this.creatureForm.value.experienceReward,
+      baseAttributes: attributes,
+    };
+
+    return newCreature;
+  }
+
+  // Method to select a creature to display its details
+  selectCreature(creature: Creature | null): void {
+    this.selectedCreature = creature;
+    if (creature) {
+      this.updateForm(creature);
+    } else {
+      this.creatureForm.reset();
+    }
+  }
+}
