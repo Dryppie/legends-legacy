fab8121bedb69429301bf892f1059a17c56300f8
Dryp <hrhoej@hotmail.com>
2025-06-15 18:05:16 +0200

    seed creatures

--- LL/src/Infrastructure/Persistence/Persistence.LL/Seeds/LLDbContextExtensions.cs
+++ LL/src/Infrastructure/Persistence/Persistence.LL/Seeds/LLDbContextExtensions.cs
@@ -27,8 +27,9 @@ public static class LLDbContextExtensions
             await SeedCreatures.SeedCreaturesData(context);
             await SeedItems.SeedItemsData(context);
             await SeedProfessions.SeedProfessionsData(context);
+#if DEBUG
             await SeedAdminData(context, hasher);
-
+#endif
             await SeedInventoryItems(context);
 
             await context.SaveChangesAsync();
