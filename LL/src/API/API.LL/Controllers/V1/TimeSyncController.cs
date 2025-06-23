cb7a63d55c00e458bf9a0264f7c0786273419406
Dryp <hrhoej@hotmail.com>
2025-06-23 20:00:28 +0200

    add timesync controller

---
+++ LL/src/API/API.LL/Controllers/V1/TimeSyncController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.LL.Controllers.V1;
+
+[AllowAnonymous]
+public class TimeSyncController : BaseController
+{
+    /// <summary>
+    /// Returns the current server time in UTC format.
+    /// </summary>
+    [HttpGet()]
+    [ProducesResponseType(typeof(DateTimeOffset), StatusCodes.Status200OK)]
+    public ActionResult<DateTimeOffset> GetCurrentTime() => Ok(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+}
