d22a7f2759b3cb72a47d45b805a6aa070d04ed2f
Dryppie <hrhoej@hotmail.com>
2025-06-23 23:56:10 +0200

    increase display for offline limit to 16 hours

--- LL/src/Presentation/ll/src/app/shared/components/session-summary-popup/session-summary-popup.component.ts
+++ LL/src/Presentation/ll/src/app/shared/components/session-summary-popup/session-summary-popup.component.ts
@@ -26,7 +26,7 @@ export class SessionSummaryPopupComponent {
     if (minutes || parts.length === 0)
       parts.push(`${minutes} minute${minutes !== 1 ? 's' : ''}`);
     let joinedDuration = parts.join(', ');
-    if (days || hours >= 12) joinedDuration += ' (Rewards stop after 12 hours)';
+    if (days || hours >= 16) joinedDuration += ' (Rewards stop after 16 hours)';
     return joinedDuration;
   }
 }
