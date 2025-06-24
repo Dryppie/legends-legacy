4383c6122f09b8736b42e9278a0cb828d082b8f0
Jakob Kjeldsteen <115626970+jkjeldsteen@users.noreply.github.com>
2025-06-24 19:33:47 +0200

    First Try at making rankings have medals

--- LL/src/Presentation/ll/src/app/features/game/city/colosseum/rankings-glory/rankings-glory.component.ts
+++ LL/src/Presentation/ll/src/app/features/game/city/colosseum/rankings-glory/rankings-glory.component.ts
@@ -1,4 +1,4 @@
-import { NgClass, NgFor, NgIf } from '@angular/common';
+import { NgClass, NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { CharacterService } from '../../../../../core/services/api/character/character.service';
 import { Subscription } from 'rxjs';
@@ -8,7 +8,7 @@ import { CharacterStateService } from '../../../../../core/services/api/characte
 @Component({
   selector: 'app-rankings-glory',
   standalone: true,
-  imports: [NgIf, NgFor, NgClass],
+  imports: [NgIf, NgFor, NgClass, NgSwitch, NgSwitchCase, NgSwitchDefault],
   templateUrl: './rankings-glory.component.html',
 })
 export class RankingsGloryComponent implements OnInit {
@@ -31,5 +31,14 @@ export class RankingsGloryComponent implements OnInit {
 
   getMyRanking(): ColosseumRank | undefined {
     return this.rankings.find((r) => r.characterId === this.id);
+  }
+
+  getMedalForRank(rank: number): string {
+    switch(rank) {
+      case 1: return 'assets/icons/medals/GoldMedalBlackOutline.svg';
+      case 2: return 'assets/icons/medals/SilverMedalBlackOutline.svg';
+      case 3: return 'assets/icons/medals/BronzeMedalBlackOutline.svg';
+      default: return '';
+    }
   }
 }
