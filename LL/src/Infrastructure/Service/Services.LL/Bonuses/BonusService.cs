c9dcee66191cc18f76600db1379726eb050a63d5
Dryp <hrhoej@hotmail.com>
2025-06-29 16:35:33 +0200

    fix db concurrency issue

--- LL/src/Infrastructure/Service/Services.LL/Bonuses/BonusService.cs
+++ LL/src/Infrastructure/Service/Services.LL/Bonuses/BonusService.cs
@@ -14,15 +14,16 @@ public sealed class BonusService : IBonusService
     public async ValueTask<IReadOnlyDictionary<BonusKind, double>> GetAggregatedAsync(Guid characterId, DateTimeOffset now, CancellationToken ct = default)
     {
         // 1. ask all providers in parallel
-        var tasks = _providers.Select(p => p.GetBonusesAsync(characterId, now, ct).AsTask());
-        var results = await Task.WhenAll(tasks);
-
-        // 2. merge by (Kind, Mode)
         var accumulator = new Dictionary<BonusKind, double>();
 
-        foreach (var bonus in results.SelectMany(b => b))
+        foreach (var provider in _providers)
         {
-            accumulator[bonus.Kind] = accumulator.GetValueOrDefault(bonus.Kind) + bonus.Value;
+            // 2. merge by (Kind, Mode)
+            var bonuses = await provider.GetBonusesAsync(characterId, now, ct);
+            foreach (var bonus in bonuses)
+            {
+                accumulator[bonus.Kind] = accumulator.GetValueOrDefault(bonus.Kind) + bonus.Value;
+            }
         }
 
         return accumulator;
