0cde88468bad768793ef5f69c445a686faece50d
Dryp <hrhoej@hotmail.com>
2025-07-04 16:37:54 +0200

    display rating gained / lost in record of battles

--- LL/src/Presentation/ll/src/app/shared/models/Dtos/colosseum/colosseumMatchResult.ts
+++ LL/src/Presentation/ll/src/app/shared/models/Dtos/colosseum/colosseumMatchResult.ts
@@ -1,8 +1,12 @@
 export interface ColosseumMatchResult {
   characterAId: string;
   characterAName: string;
+  characterARatingBefore: number;
+  characterARatingAfter: number;
   characterBId: string;
   characterBName: string;
+  characterBRatingBefore: number;
+  characterBRatingAfter: number;
   winnerId: string;
   winnerName: string;
   playedAt: Date;
