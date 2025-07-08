b23bce3feea6655d074108e71a59a2ccb37c5d77
Dryp <hrhoej@hotmail.com>
2025-07-08 08:11:38 +0200

    add amulets

--- LL/src/Core/Domain/Models/Attributes/AttributeType.cs
+++ LL/src/Core/Domain/Models/Attributes/AttributeType.cs
@@ -2,19 +2,19 @@
 public enum AttributeType
 {
     /* ===== VITALITY ===== */
-    MaxHealth, // 5k HP maxed out from CON and END
+    MaxHealth,
     Health,
-    HealthRegeneration, // 150 Regen every 5 Tick maxed out from CON and FIG
-    MaxMana, // 2.5k MP maxed out from INT and WIS
+    HealthRegeneration,
+    MaxMana,
     Mana,
-    ManaRegeneration, // 150 Regen every 5 Tick maxed out from WIL and WIS
+    ManaRegeneration,
     RecoveryRate,
     Barrier, // Can't be more than 2x Health
 
     /* ===== OFFENSE =====*/
     AttackPower,
     SpellPower,
-    AttackSpeed, // +20 maxed out from AGI - Equals 50 increments per tick, resulting in 6 ticks instead of 10 to perform an attack
+    AttackSpeed,
     Accuracy,
     CritChance,
     CritDamage,
@@ -24,8 +24,8 @@ public enum AttributeType
     ManaPenetration,
 
     /* ===== DEFENSE ===== */
-    PhysicalDefense, // 100 maxed out from END - Primarily gotten through Essences and Equipment
-    MagicalDefense, // 100 maxed out from WIL - Primarily gotten through Essences and Equipment
+    PhysicalDefense,
+    MagicalDefense,
     DamageReduction,
     CritDamageReduction, // Reduces damage dealt by a crit
     CrowdControlResistance, // CC
