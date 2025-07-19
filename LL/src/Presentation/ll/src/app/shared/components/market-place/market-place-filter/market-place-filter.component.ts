4cc62b1fb9fa372af24304f58c6b8617847733e4
Dryp <hrhoej@hotmail.com>
2025-07-19 17:06:11 +0200

    add market filter

---
+++ LL/src/Presentation/ll/src/app/shared/components/market-place/market-place-filter/market-place-filter.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-market-place-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './market-place-filter.component.html',
+})
+export class MarketPlaceFilterComponent {}
