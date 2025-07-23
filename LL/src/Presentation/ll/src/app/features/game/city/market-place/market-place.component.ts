4211d358fc0d8333d16d4e86af4831ca39cac0e3
Dryp <hrhoej@hotmail.com>
2025-07-23 21:36:36 +0200

    initial setup for new market place filter

--- LL/src/Presentation/ll/src/app/features/game/city/market-place/market-place.component.ts
+++ LL/src/Presentation/ll/src/app/features/game/city/market-place/market-place.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { BannerComponent } from '../../../../shared/components/banner/banner.component';
 import { TabComponent } from '../../../../shared/components/tabs/tab/tab.component';
 import { MarketPlaceBuyComponent } from './market-place-buy/market-place-buy.component';
 import { MarketPlaceSellComponent } from './market-place-sell/market-place-sell.component';
 import { TabsComponent } from '../../../../shared/components/tabs/tabs.component';
 import { MarketPlaceGenericComponent } from '../../../../shared/components/market-place/market-place-generic/market-place-generic.component';
+import { MarketPlaceFilterComponent } from '../../../../shared/components/market-place/market-place-filter/market-place-filter.component';
+import { ItemType } from '../../../../shared/models/enums/itemType';
+import { NgSwitch, NgSwitchCase } from '@angular/common';
 
 @Component({
   selector: 'app-market-place',
@@ -17,7 +20,17 @@ import { MarketPlaceGenericComponent } from '../../../../shared/components/marke
     TabsComponent,
     TabComponent,
     MarketPlaceGenericComponent,
+    MarketPlaceFilterComponent,
+    NgSwitch,
+    NgSwitchCase,
   ],
   templateUrl: './market-place.component.html',
 })
-export class MarketPlaceComponent {}
+export class MarketPlaceComponent {
+  readonly ItemType = ItemType;
+  readonly selectedItemType = signal<ItemType>(ItemType.Equipment);
+
+  onItemTypeChanged(type: ItemType) {
+    this.selectedItemType.set(type);
+  }
+}
--- LL/src/Presentation/ll/src/app/shared/components/market-place/market-place-filter/market-place-filter.component.ts
+++ LL/src/Presentation/ll/src/app/shared/components/market-place/market-place-filter/market-place-filter.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { NgClass, NgFor } from '@angular/common';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
+import { ItemType } from '../../../models/enums/itemType';
 
 @Component({
   selector: 'app-market-place-filter',
   standalone: true,
-  imports: [],
+  imports: [NgClass, NgFor],
   templateUrl: './market-place-filter.component.html',
 })
-export class MarketPlaceFilterComponent {}
+export class MarketPlaceFilterComponent {
+  readonly itemTypes = [
+    ItemType.Material,
+    ItemType.Consumable,
+    ItemType.Equipment,
+    ItemType.Essence,
+  ];
+
+  readonly selectedItemType = signal<ItemType>(ItemType.Equipment);
+
+  @Output() readonly itemTypeChanged = new EventEmitter<ItemType>();
+
+  setSelectedType(type: ItemType) {
+    this.selectedItemType.set(type);
+    this.itemTypeChanged.emit(type);
+  }
+}
