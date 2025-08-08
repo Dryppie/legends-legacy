1d0f0fb243a3530ca1b790e3786b4b90bf0bc29f
Dryp <hrhoej@hotmail.com>
2025-08-08 07:39:38 +0200

    initial search field to character overview page

--- LL/src/Presentation/ll/src/app/features/game/character/character-overview/character-overview.component.ts
+++ LL/src/Presentation/ll/src/app/features/game/character/character-overview/character-overview.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { CharacterService } from '../../../../core/services/api/character/character.service';
+import { CharacterAttributesComponent } from '../../../../shared/components/character/character-attributes/character-attributes.component';
 import { DefaultHeaderComponent } from '../../../../shared/components/default-header/default-header.component';
 import { EquippedEssencesComponent } from '../../../../shared/components/essences/equipped-essences/equipped-essences.component';
-import { CharacterService } from '../../../../core/services/api/character/character.service';
 import { CharacterOverviewDto } from '../../../../shared/models/Dtos/characterDto';
-import { Observable } from 'rxjs';
-import { CharacterAttributesComponent } from '../../../../shared/components/character/character-attributes/character-attributes.component';
-import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-character-overview',
@@ -16,14 +17,32 @@ import { AsyncPipe, NgIf } from '@angular/common';
     CharacterAttributesComponent,
     AsyncPipe,
     NgIf,
+    FormsModule,
   ],
   templateUrl: './character-overview.component.html',
 })
 export class CharacterOverviewComponent {
   readonly character$!: Observable<CharacterOverviewDto>;
 
+  searchValue = '';
+
+  @Output() search = new EventEmitter<string>();
+
   constructor(private characterService: CharacterService) {
     // `characterService` is already injected → safe to use here
     this.character$ = this.characterService.characterOverview$;
+  }
+
+  onSearch() {
+    const trimmed = this.searchValue.trim();
+    if (trimmed) {
+      this.search.emit(trimmed);
+    }
+  }
+
+  onEnter(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.onSearch();
+    }
   }
 }
