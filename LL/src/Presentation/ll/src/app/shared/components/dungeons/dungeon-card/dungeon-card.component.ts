826ae1c43a39f23409c9e37fc14de4d3b056bd86
Dryp <hrhoej@hotmail.com>
2025-08-12 21:27:24 +0200

    improve dungeon card design

--- LL/src/Presentation/ll/src/app/shared/components/dungeons/dungeon-card/dungeon-card.component.ts
+++ LL/src/Presentation/ll/src/app/shared/components/dungeons/dungeon-card/dungeon-card.component.ts
@@ -1,10 +1,15 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
 import { RegularButtonComponent } from '../../custom-components/buttons/regular-button/regular-button.component';
+import {
+  DungeonPreviewComponent,
+  DungeonPreviewData,
+} from '../dungeon-preview/dungeon-preview.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-dungeon-card',
   standalone: true,
-  imports: [RegularButtonComponent],
+  imports: [NgIf, DungeonPreviewComponent, RegularButtonComponent],
   templateUrl: './dungeon-card.component.html',
 })
 export class DungeonCardComponent {
@@ -18,9 +23,37 @@ export class DungeonCardComponent {
   @Input() height = 176;
   @Input() cornerSize = 32;
 
-  @Output() enter = new EventEmitter<void>();
+  // In a real app, fetch this by id. Hard-coded example for Goblin Mines:
+  previewData: DungeonPreviewData = {
+    id: 'goblin-mines',
+    title: 'Goblin Mines',
+    heroImage: 'entities/optimized/hobgoblin.webp',
+    lore: 'The goblins have mined deep into cursed stone, guarding ancient relics.',
+    requiredLevel: 5,
+    dailyEntries: 1,
+    keyItem: {
+      icon: '/assets/icons/key-goblin.svg',
+      name: 'Goblin Key',
+      have: 0,
+      need: 1,
+    },
+    roomsRange: [5, 8],
+    estMinutes: [3, 5],
+    unlockedDifficulties: ['Normal'], // only Normal unlocked in this example
+    rewards: [
+      { icon: '/assets/icons/sword.svg', name: 'Goblin Cleaver' },
+      { icon: '/assets/icons/gem-green.svg', name: 'Viridian Shard' },
+      { icon: '/assets/icons/scroll.svg', name: 'Rune Scroll' },
+      { icon: '/assets/icons/essence-purple.svg', name: 'Goblin Essence' },
+    ],
+  };
 
-  onEnter(): void {
-    this.enter.emit();
+  showPreview = signal(false);
+
+  openPreview() {
+    this.showPreview.set(true);
+  }
+  closePreview() {
+    this.showPreview.set(false);
   }
 }
