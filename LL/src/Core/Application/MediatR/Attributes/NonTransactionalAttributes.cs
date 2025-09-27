3f25bd7e234293afcc7302b393a27eaa6d148d86
Dryp <hrhoej@hotmail.com>
2025-09-27 15:39:40 +0200

    comment for non transactional commands

--- LL/src/Core/Application/MediatR/Attributes/NonTransactionalAttributes.cs
+++ LL/src/Core/Application/MediatR/Attributes/NonTransactionalAttributes.cs
@@ -1,3 +1,5 @@
 ﻿namespace Application.MediatR.Attributes;
 [AttributeUsage(AttributeTargets.Class)]
 public sealed class NonTransactionalAttribute : Attribute { }
+
+// Add [NonTransactional] to a command to opt out of the transaction behavior
