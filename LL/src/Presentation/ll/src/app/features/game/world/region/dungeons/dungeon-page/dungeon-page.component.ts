0759ec519bdf7409cd0696750723524c1aba7560
Dryp <hrhoej@hotmail.com>
2026-06-09 10:54:00 +0200

    list pending items

--- LL/src/Presentation/ll/src/app/features/game/world/region/dungeons/dungeon-page/dungeon-page.component.ts
+++ LL/src/Presentation/ll/src/app/features/game/world/region/dungeons/dungeon-page/dungeon-page.component.ts
@@ -153,13 +153,6 @@ export class DungeonPageComponent {
     return this.activeDungeon()?.pendingRewards ?? [];
   });
 
-  readonly pendingItemQuantity = computed(() => {
-    return this.pendingRewards().reduce(
-      (total, reward) => total + (reward.quantity ?? 0),
-      0,
-    );
-  });
-
   readonly completedRooms = computed(() => {
     const run = this.activeDungeon();
     if (!run?.rooms?.length) return [];
