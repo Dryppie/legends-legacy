a4123ec0a4d960104fc3588f70e94301e3c288bc
Dryp <hrhoej@hotmail.com>
2026-06-13 00:51:55 +0200

    fix token issue

--- LL/src/Presentation/ll/src/app/shared/components/default-header/default-header.component.ts
+++ LL/src/Presentation/ll/src/app/shared/components/default-header/default-header.component.ts
@@ -1,14 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { ProfessionIconComponent } from '../professions/profession-icon/profession-icon.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-default-header',
   standalone: true,
-  imports: [ProfessionIconComponent],
+  imports: [NgIf, ProfessionIconComponent],
   templateUrl: './default-header.component.html',
 })
 export class DefaultHeaderComponent {
   @Input() title: string = '';
   @Input() text: string = '';
   @Input() icon: string = '';
+  @Input() section: string = '';
 }
