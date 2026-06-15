2543dc0c8ac7fd2d1ae17002d478663c6f18daa1
Dryp <hrhoej@hotmail.com>
2026-06-16 00:07:01 +0200

    add component for notification indicator

--- LL/src/Presentation/ll/src/app/layout/dashboard/sidebar/sidebar-item/sidebar-item.component.ts
+++ LL/src/Presentation/ll/src/app/layout/dashboard/sidebar/sidebar-item/sidebar-item.component.ts
@@ -1,12 +1,12 @@
-import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Tab } from '../../../../shared/models/sidebar-item';
 import { ProfessionIconComponent } from '../../../../shared/components/professions/profession-icon/profession-icon.component';
+import { NotificationIndicatorComponent } from '../../../../shared/components/notification-indicator/notification-indicator.component';
 
 @Component({
   selector: 'app-sidebar-item',
   standalone: true,
-  imports: [NgIf, ProfessionIconComponent],
+  imports: [ProfessionIconComponent, NotificationIndicatorComponent],
   templateUrl: './sidebar-item.component.html',
 })
 export class SidebarItemComponent {
