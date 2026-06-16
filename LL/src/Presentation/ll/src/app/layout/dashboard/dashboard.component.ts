782f3abf96e20af36c50b9c03e01762c13bc0b9a
Dryp <hrhoej@hotmail.com>
2026-06-16 21:46:54 +0200

    improve current action for mobile

--- LL/src/Presentation/ll/src/app/layout/dashboard/dashboard.component.ts
+++ LL/src/Presentation/ll/src/app/layout/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, effect, HostListener, OnInit } from '@angular/core';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AsyncPipe, NgClass, NgIf } from '@angular/common';
 import { Observable } from 'rxjs';
@@ -8,6 +8,11 @@ import { GameService } from '../../core/services/client-side/game/game.service';
 import { CombatComponent } from '../../shared/components/combat/combat.component';
 import { ChatComponent } from './chat/chat.component';
 import { LootTrackerComponent } from './loot-tracker/loot-tracker.component';
+import { CurrentActionComponent } from '../../shared/components/current-action/current-action.component';
+import { CharacterActionsStateService } from '../../core/services/api/character-actions/character-actions.state.service';
+import { CharacterActionType } from '../../shared/models/enums/characterActionType';
+import { Equipment } from '../../shared/models/item';
+import { EquipmentType } from '../../shared/models/enums/equipmentType';
 
 @Component({
   selector: 'app-dashboard',
@@ -22,6 +27,7 @@ import { LootTrackerComponent } from './loot-tracker/loot-tracker.component';
     CombatComponent,
     ChatComponent,
     LootTrackerComponent,
+    CurrentActionComponent,
   ],
   templateUrl: './dashboard.component.html',
 })
@@ -31,9 +37,18 @@ export class DashboardComponent implements OnInit {
   isScreenLarge = false;
   isChatOpenDesktop = true; // open by default on ≥ lg
   isFloatingChatOpen = false;
+  displayCurrentAction = false;
   combatVisible$!: Observable<boolean>;
 
-  constructor(private gameService: GameService) {}
+  constructor(
+    private readonly gameService: GameService,
+    private readonly state: CharacterActionsStateService,
+    private readonly router: Router,
+  ) {
+    effect(() => {
+      this.displayCurrentAction = this.state.displayCurrentAction();
+    });
+  }
 
   ngOnInit() {
     this.checkScreenSize();
@@ -85,5 +100,60 @@ export class DashboardComponent implements OnInit {
     if (this.isScreenSmall) {
       this.isSidebarOpen = false;
     }
+  }
+
+  navigateToAction(): void {
+    const action = this.state.currentAction();
+    if (!action) return;
+
+    const actionType = action.characterActionType;
+    const now = Date.now();
+    const updatedAt = new Date(action.updatedAt ?? 0).getTime();
+
+    if (updatedAt > now) {
+      this.gameService.showCombat();
+      return;
+    }
+
+    let route: string[] = [];
+
+    if (actionType === CharacterActionType.Gathering) {
+      route = [
+        'game',
+        'professions',
+        'gathering',
+        action.gatheringActionDetails!.professionType.toLowerCase(),
+      ];
+    } else {
+      const equipmentType = (
+        action.craftingActionDetails?.craftingQueueItems[0].equipmentInstance
+          .itemBase as Equipment
+      ).equipmentType;
+
+      switch (equipmentType) {
+        case EquipmentType.Head:
+        case EquipmentType.Chest:
+        case EquipmentType.Legs:
+          route = ['game', 'professions', 'crafting', 'armorforging'];
+          break;
+
+        case EquipmentType.TwoHanded:
+        case EquipmentType.OneHanded:
+        case EquipmentType.OffHand:
+          route = ['game', 'professions', 'crafting', 'weaponsmithing'];
+          break;
+
+        case EquipmentType.Relic:
+        case EquipmentType.Necklace:
+        case EquipmentType.Ring:
+          route = ['game', 'professions', 'crafting', 'jewelrycrafting'];
+          break;
+
+        default:
+          return;
+      }
+    }
+
+    this.router.navigate(route);
   }
 }
