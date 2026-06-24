8bac1ce1261741e811854f5da1ee786f2be434d2
Dryp <hrhoej@hotmail.com>
2026-06-24 20:28:16 +0200

    remove old legacy crafting code

--- LL/src/Core/Application/Interfaces/Services/LL/Professions/IRecipeService.cs
+++
@@ -1,7 +0,0 @@
-﻿using Domain.Models.Professions.Crafting;
-
-namespace Application.Interfaces.Services.LL.Professions;
-public interface IRecipeService
-{
-    Task<Recipe?> GetRecipeByIdAsync(Guid recipeId, CancellationToken cancellationToken);
-}
