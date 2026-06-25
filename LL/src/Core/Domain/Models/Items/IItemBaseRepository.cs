ac2e9115236a17038cb872fd91836bb6e0819001
Dryp <hrhoej@hotmail.com>
2026-06-25 14:52:59 +0200

    item base repo

--- LL/src/Core/Domain/Models/Items/IItemBaseRepository.cs
+++ LL/src/Core/Domain/Models/Items/IItemBaseRepository.cs
@@ -3,4 +3,6 @@ namespace Domain.Models.Items;
 public interface IItemBaseRepository
 {
     Task<IReadOnlyDictionary<string, ItemBase>> GetItemBasesByIdsAsync(IReadOnlyCollection<string> itemIds, CancellationToken cancellationToken);
+    Task<IReadOnlyDictionary<string, string>> GetEssenceItemBaseIdsByDefinitionIdAsync(CancellationToken cancellationToken);
+    Task<Equipments.EquipmentBase?> GetCraftableEquipmentBaseAsync(string itemBaseId, CancellationToken cancellationToken);
 }
