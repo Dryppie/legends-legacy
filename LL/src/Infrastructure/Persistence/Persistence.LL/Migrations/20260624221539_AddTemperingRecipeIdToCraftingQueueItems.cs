f448e804f573c31f6218d765f6f5cc55ed6701ed
Dryp <hrhoej@hotmail.com>
2026-06-25 14:36:36 +0200

    migrations

---
+++ LL/src/Infrastructure/Persistence/Persistence.LL/Migrations/20260624221539_AddTemperingRecipeIdToCraftingQueueItems.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.LL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTemperingRecipeIdToCraftingQueueItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TemperingRecipeId",
+                table: "CraftingQueueItems",
+                type: "character varying(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TemperingRecipeId",
+                table: "CraftingQueueItems");
+        }
+    }
+}
