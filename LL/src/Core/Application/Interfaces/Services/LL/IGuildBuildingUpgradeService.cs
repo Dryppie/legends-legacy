cc0adcd24d9a90a7adfe08284f93b012cd087fea
Dryp <hrhoej@hotmail.com>
2026-06-27 16:46:52 +0200

    remove old guild building system

--- LL/src/Core/Application/Interfaces/Services/LL/IGuildBuildingUpgradeService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Domain.Models.Guilds.Buildings;
-
-namespace Application.Interfaces.Services.LL;
-public interface IGuildBuildingUpgradeService
-{
-    Task<List<BuildingUpgradeView>> GetForGuildAsync(Guid characterId, CancellationToken cancellationToken);
-    Task<bool> PurchaseAsync(Guid characterId, string upgradeId, CancellationToken cancellationToken);
-}
