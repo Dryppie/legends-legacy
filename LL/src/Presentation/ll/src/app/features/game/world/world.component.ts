51d57b15ad41ca6a2633dc8a625271cbf080b9fb
Dryp <hrhoej@hotmail.com>
2026-08-12 15:29:13 +0200

    fix dungeon scroll

--- LL/src/Presentation/ll/src/app/features/game/world/world.component.ts
+++ LL/src/Presentation/ll/src/app/features/game/world/world.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-world',
-    imports: [RouterOutlet],
-    templateUrl: './world.component.html'
+  selector: 'app-world',
+  imports: [RouterOutlet],
+  templateUrl: './world.component.html',
+  host: {
+    class: 'block h-full min-h-0',
+  },
 })
 export class WorldComponent {}
